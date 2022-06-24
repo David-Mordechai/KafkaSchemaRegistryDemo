236c7df1d853fe1c1a047c82fcb8f0c2a204fcf1
David Mordechai <david.mordechai79@gmail.com>
2022-06-24 18:06:37 +0300

    Add project files.

---
+++ Publisher.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using ProtoDtos;
+
+const string bootstrapServers = "localhost:9092";
+const string schemaRegistryUrl = "localhost:8081";
+const string topicName = "users";
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = bootstrapServers
+};
+
+var schemaRegistryConfig = new SchemaRegistryConfig
+{
+    // Note: you can specify more than one schema registry url using the
+    // schema.registry.url property for redundancy (comma separated list). 
+    // The property name is not plural to follow the convention set by
+    // the Java implementation.
+    Url = schemaRegistryUrl,
+};
+
+using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+using var producer = new ProducerBuilder<string, User>(producerConfig)
+        .SetValueSerializer(new ProtobufSerializer<User>(schemaRegistry))
+        .Build();
+Console.WriteLine($"{producer.Name} producing on {topicName}. Enter user names, q to exit.");
+
+var i = 0;
+string? text;
+while ((text = Console.ReadLine()) != "q")
+{
+    var user = new User { FullName = text, FavoriteColor = "Green", FavoriteNumber = i++ };
+    var result = await producer
+        .ProduceAsync(topicName, new Message<string, User> { Key = text ?? string.Empty, Value = user })
+        .ContinueWith(task => task.IsFaulted
+            ? $"error producing message: {task.Exception?.Message}"
+            : $"produced to: {task.Result.TopicPartitionOffset}");
+
+    Console.WriteLine(result);
+}
---
+++ Subsrciber.Console/Program.cs
@@ -0,0 +1,61 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using ProtoDtos;
+
+const string bootstrapServers = "localhost:9092";
+const string schemaRegistryUrl = "localhost:8081";
+const string topicName = "users";
+
+var schemaRegistryConfig = new SchemaRegistryConfig
+{
+    // Note: you can specify more than one schema registry url using the
+    // schema.registry.url property for redundancy (comma separated list). 
+    // The property name is not plural to follow the convention set by
+    // the Java implementation.
+    Url = schemaRegistryUrl,
+};
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = bootstrapServers,
+    GroupId = "protoBuf-example-consumer-group"
+};
+
+var cts = new CancellationTokenSource();
+var consumeTask = Task.Run(() =>
+{
+    using var consumer =
+        new ConsumerBuilder<string, User>(consumerConfig)
+            .SetValueDeserializer(new ProtobufDeserializer<User>().AsSyncOverAsync())
+            .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+            .Build();
+    consumer.Subscribe(topicName);
+
+    try
+    {
+        while (true)
+        {
+            try
+            {
+                var consumeResult = consumer.Consume(cts.Token);
+                Console.WriteLine(
+                    $"user name: {consumeResult.Message.Key}, " +
+                    $"favorite color: {consumeResult.Message.Value.FavoriteColor}, " +
+                    $"favorite number {consumeResult.Message.Value.FavoriteNumber}");
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Consume error: {e.Error.Reason}");
+            }
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        consumer.Close();
+    }
+});
+
+Console.ReadKey();
+cts.Cancel();
